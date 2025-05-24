3e7cc141e6a5db66236fdc849a2e89de88cf81df
jaimevs20 <jaimevalverde.jv@gmail.com>
2025-05-24 19:31:36 -0300

    Adicionada data para agrupamento

--- app/src/main/java/com/kotlin_app/gastospush/data/model/NotificacaoPush.kt
+++ app/src/main/java/com/kotlin_app/gastospush/data/model/NotificacaoPush.kt
@@ -1,7 +1,10 @@
 package com.kotlin_app.gastospush.data.model
 
+import java.util.Date
+
 data class NotificacaoPush (
     val titulo : String,
     val texto : String,
-    val valorItem: Double
+    val valorItem: Double,
+    val dataLancamento: Date
 )
